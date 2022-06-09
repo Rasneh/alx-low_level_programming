1329149c113831d57ca480219123458322d67a7e
Rasneh <105076463+Rasneh@users.noreply.github.com>
2022-06-08 20:38:02 -0900

    Update 3-main_b.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -14,10 +14,10 @@ int day;
 int year;
 
 month = 2;
-day = 28;
+day = 29;
 year = 2000;
 
-printf("Date: %02d/%02d/%04d\n", month, day , year);
+printf("Date: %04d/%02d/%02d\n", month, day , year);
 
 day = convert_day(month, day);
 
